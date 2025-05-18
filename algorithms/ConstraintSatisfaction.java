729baec154f2208f9fe2b892106e3c4aa82ac691
mnnhQuan <s4032589@rmit.edu.vn>
2025-05-18 22:24:17 +0700

    update

--- algorithms/ConstraintSatisfaction.java
+++ algorithms/ConstraintSatisfaction.java
@@ -55,7 +55,7 @@ public class ConstraintSatisfaction implements RMIT_Sudoku_Solver {
         return false;
     }
 
-    // Average Time Complexity: O(81) = O(1)
+    // Average Time Complexity: O(1)
     // Iterate through the sudoku board to find most
     // constrained cells with fewest possible values
     private static int[] findMostConstrainedCell(int[][] board) {
@@ -77,13 +77,12 @@ public class ConstraintSatisfaction implements RMIT_Sudoku_Solver {
         return result;
     }
 
-    // Average Time Complexity: O(27) = O(1)
+    // Average Time Complexity: O(1)
     // Return a list of possible values by checking
     // row, column and 3x3 sub-grid constrain
     private static List<Integer> getPossibleValues(int[][] board, int row, int col) {
         boolean[] used = new boolean[GRID_SIZE + 1];
 
-        // Time Complexity: O(9)
         // Mark numbers used in the row
         for (int i = 0; i < GRID_SIZE; i++) {
             if (board[row][i] != 0) {
@@ -91,7 +90,6 @@ public class ConstraintSatisfaction implements RMIT_Sudoku_Solver {
             }
         }
 
-        // Time Complexity: O(9)
         // Mark numbers used in the column
         for (int i = 0; i < GRID_SIZE; i++) {
             if (board[i][col] != 0) {
@@ -99,7 +97,6 @@ public class ConstraintSatisfaction implements RMIT_Sudoku_Solver {
             }
         }
 
-        // Time Complexity: O(9)
         // Mark numbers used in the 3x3 subgrid
         int localBoxRow = row - row % 3;
         int localBoxCol = col - col % 3;
@@ -110,7 +107,7 @@ public class ConstraintSatisfaction implements RMIT_Sudoku_Solver {
                 }
             }
         }
-        // Average Time Complexity: O(1)
+
         // Collect all unused numbers
         List<Integer> possibleValues = new ArrayList<>();
         for (int num = 1; num <= GRID_SIZE; num++) {
